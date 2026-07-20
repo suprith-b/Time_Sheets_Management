0cf6394acb9fe49ea9933493cdc6c94c7331de81
suprith-b <suprith.b@tektalis.com>
2026-07-20 17:44:10 +0530

    changed db schema

--- backend/app/models/UserModel.py
+++ backend/app/models/UserModel.py
@@ -1,12 +1,14 @@
-from sqlalchemy import Column, Integer, String, Foreignkey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from app.db.database import Base
 
-class User( Base ):
+
+class User(Base):
     __tablename__ = "users"
 
-    id = Column( Integer, primary_key = True, autoincrement = True )
-    username = Column( String( 30 ) )
-    role_id = Column( Integer, Foreignkey( "roles.id" ) )
-    name = Column( String( 50 ) )
-    email = Column( String( 50 ) )
-    password = Column( String( 60 ) )
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    role_id = Column(Integer, ForeignKey("roles.id"))
+    manager_id = Column(Integer, ForeignKey("users.id"))
+    username = Column(String(30))
+    name = Column(String(50))
+    email = Column(String(50))
+    password = Column(String(60))
